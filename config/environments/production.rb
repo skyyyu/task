e15c31eaea5b31949577a52ccf9434b9cd4cf895
関祐樹 <greenpeace623@docomo.ne.jp>
2019-05-31 14:53:38 +0900

    mail fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,18 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "seki.yyy22@gmail.com",
+    :password => "seki8623",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
